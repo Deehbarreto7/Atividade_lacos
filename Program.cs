6dd82ea5536b7f8871208ae41aae2e3a26a005a8
Débora <deehbarreto7@gmail.com>
2026-05-22 20:42:12 -0300

    atividade feita em sala de aula

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Quantas vezes quer exibir? ");
+int maximo = Convert.ToInt32(Console.ReadLine());
+
+int contador = 0;
+
+while (contador < maximo)
+{
+    if (contador % 2 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+    }
+    int repeticoes = 0;
+    while(repeticoes < 3)
+    {
+        Console.Write("Hello, World! ");
+        repeticoes = repeticoes + 1;
+    }
+
+    contador = contador + 1;
+
+    Console.WriteLine(contador);
+}
+
+Console.ResetColor();
